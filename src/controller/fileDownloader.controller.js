2037847330fdccc90e2e701244e1379d16946889
Khushab <khushab.alam@yocket.in>
2022-07-23 19:45:45 +0530

    added number of retries option

--- src/controller/fileDownloader.controller.js
+++ src/controller/fileDownloader.controller.js
@@ -1,27 +1,53 @@
 const { downloadFileForHTTP, downloadFileForHTTPS } = require('../services/fileDownloader.service')
 const path = require('path');
 
-function downloadFromSingleURL(url, location) {
-    const protocol = url.split("://")[0]
-    const fileName = path.basename(url)
+function downloadFromSingleURL(body) {
+    let retryCount = body.retries || 1;
+    const protocol = body.url.split("://")[0]
+    const fileName = path.basename(body.url)
     const payload = {
-        url,
+        url: body.url,
         fileName
     }
-    switch (protocol) {
-        case 'http':
-            return downloadFileForHTTP(payload)
-        case 'https':
-            return downloadFileForHTTPS(payload)
-        default:
-            return downloadFileForHTTPS(payload)
+    const response = downloadFileAsPerProtocol(protocol, payload)
+    if (!response.state) {
+        if (retryCount !== 0) {
+            console.log("Download failed!")
+            console.log("Retrying...")
+            retryCount -= 1
+            downloadFileAsPerProtocol(protocol, payload)
+        }
+        else {
+            throw new Error(error)
+        }
     }
 }
 
-function downloadFromMultipleURL(url, location) {
-    url.forEach((item) => {
-        downloadFromSingleURL(item, location)
+function downloadFromMultipleURL(body) {
+    body.url.forEach((item) => {
+        downloadFromSingleURL({ ...body, url: item })
     })
+}
+
+function downloadFileAsPerProtocol(protocol, payload) {
+    const response = {}
+    try {
+        switch (protocol) {
+            case 'http':
+                downloadFileForHTTP(payload)
+                break;
+            case 'https':
+                downloadFileForHTTPS(payload)
+                break;
+            default:
+                downloadFileForHTTPS(payload)
+                break;
+        }
+        response.state = true
+    } catch (error) {
+        response.state = false
+    }
+    return response
 }
 
 module.exports = { downloadFromSingleURL, downloadFromMultipleURL }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 const { downloadFromSingleURL, downloadFromMultipleURL } = require('./controller/fileDownloader.controller')
 
 
-function fileDownloader(url, location) {
-    if (Array.isArray(url)) {
-        downloadFromMultipleURL(url, location)
+function fileDownloader(body) {
+    if (Array.isArray(body.url)) {
+        downloadFromMultipleURL(body)
     } else {
-        downloadFromSingleURL(url, location)
+        downloadFromSingleURL(body)
     }
 }
 const url1 = "http://www.wikidot.com/common--theme/wikidot30/images/how3.png"
 const url2 = "https://images.pexels.com/photos/12270237/pexels-photo-12270237.jpeg"
 
-fileDownloader([url2, url1])
+const body = {
+    url: [url2, url1],
+    retries: 2
+}
+
+fileDownloader(body)
 // fileDownloader(url1)
 // fileDownloader(url2)
 
